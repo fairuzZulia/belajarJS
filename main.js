45361f1b9bff0ea2282cab4c1d972b8e3c3be384
Fairuz Zulia Muntaha <144518441+fairuzZulia@users.noreply.github.com>
2023-10-12 17:08:45 +0700

    Update main.js

--- main.js
+++ main.js
@@ -59,21 +59,30 @@
 //         document.write( e = "False");
 // }
 
-let x = 6;
-let y = 3;
+// let x = 6;
+// let y = 3;
 
-console.log(x<10 && y>1);
-console.log(x<10 && y<1);
-console.log(x==5||y==5);
-console.log(x==6||y==5);
-console.log(!(x==y));
+// console.log(x<10 && y>1);
+// console.log(x<10 && y<1);
+// console.log(x==5||y==5);
+// console.log(x==6||y==5);
+// console.log(!(x==y));
 
-document.write(x<10 && y>1);
-document.write("<br>");
-document.write(x<10 && y<1);
-document.write("<br>");
-document.write(x==5||y==5);
-document.write("<br>");
-document.write(x==6||y==5);
-document.write("<br>");
-document.write(!(x==y));
+// document.write(x<10 && y>1);
+// document.write("<br>");
+// document.write(x<10 && y<1);
+// document.write("<br>");
+// document.write(x==5||y==5);
+// document.write("<br>");
+// document.write(x==6||y==5);
+// document.write("<br>");
+// document.write(!(x==y));
+
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
